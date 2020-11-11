d3555fc8e1d7823684b4e383078147073d7d8fe2
hanishkvc <hanishkvc@gmail.com>
2020-11-12 00:58:53 +0530

    Minimal time required to cover earth, 1st take
    Added calc to find the minimal time that would be required to try
    and cover the earth. It tries to find how many orbits at a minimum
    is required to cover the full of earth's equator.
    
    It doesnt worry about how much of ground can be functionaly/practically
    serviced by the satellite, in each orbit. Nor does it worry about how
    the orbits may overlap or not and other nitty gritties.
    
    NOTE: The overly crude eclipse logic added yesterday just gives the
    a kind of maximal outer limit wrt eclipse time, the real time should
    be much smaller.
    
    NOTE: As can be noted above, for one of the parameters I have tried
    to get the minimalest time required, while for the other I have
    tried to get the maximal time required. And these are using crude/
    simpel minded geometric calcs.
    
    NOTE: Had forgotten to add the jupyter notebook yesterday, have
    fixed it today.

--- MyAmSatInStem.py
+++ MyAmSatInStem.py
@@ -9,6 +9,18 @@
 # Which inturn can be tracked and listened to by students at heart by using either a suitable FM receiver
 # or better still a SDR reciever.
 # 
+# ### Digital data comm
+# 
+# In turn use a simple mechanism to transmit the sensor data like
+# 
+# a) Blind Analog frequency modulation: Different frequencies for different values
+# 
+# b) Analog frequency modulated tones for different decimal digits from 0 to 9, maybe each 200 Hz appart.
+# 
+# b) Binary Digital Keying: Presence or absence of Frequency or Toggle btw 2 different sets of frequencies indicate between 0 and 1. 
+# 
+# ### Orbit
+# 
 # The Polar orbit should allow the satellite to cover all parts of the earth.
 # 
 # If a orbit altitude is reached such that the velocity achieved can ensure that N (1 or more) full orbits can be finished in time T, such that T is 24 hours or its multiples, then the satellite will retrace the same path at the same relative time once every T hours i.e T/24 days. NOTE: Not sure if the LEO orbits is sufficiently stable enough over a long period of atleast few weeks, so that it can be used to have a simple tracking wrt the satellite for that period.
@@ -19,57 +31,92 @@
 # 
 # * if it is 0 degrees, then the satellite would be always at around roughly mid day or mid night wrt the places on earth, depending on which side of the earth wrt sun, that place is.
 # 
+
+# ## NOTE
+# 
+# Below are few simple minded calculations to get a very very rough initial idea of few things
 # 
 
-# In[ ]:
+# In[101]:
 
 
 import math
+import numpy
+
+
+# ## Earth
+
+# In[102]:
+
+
+# Gravitational Constant (m^3 / (kg * s^2))
+iGravitationalConstant = (6.674 / 10**11)
+
+# Earth Radius (m)
+iEarthRadius = 6_400_000
+
+# Earth Mass (kg)
+iEarthMass = 5.972 * 10**24
+
+# Earth Circumference
+iEarthCircumference = 2 * math.pi * iEarthRadius
+print("EarthCircumference:", iEarthCircumference)
 
 
 # ## LEO
 
-# In[8]:
+# In[103]:
 
-
-## Gravitational Constant (m^3 / (kg * s^2))
-iGravitationalConstant = (6.674 / 10**11)
-
-## Earth Radius (m)
-iEarthRadius = 6_400_000
-## Earth Mass (kg)
-iEarthMass = 5.972 * 10**24
 
 ## LowEarthOrbit
+
 iLeoAltitudeAboveGround = 500_000
 iLeoAltitudeFromEarthCenter = iEarthRadius + iLeoAltitudeAboveGround
 print("LEO orbit altitude wrt Earth Center:", iLeoAltitudeFromEarthCenter)
 
-## LEO Velocity
+# LEO Velocity
 #F = (GMm)/(r^2) = ma = m(v^2/r) => GM = r(v^2)
 iLeoVelocity = math.sqrt((iGravitationalConstant * iEarthMass)/iLeoAltitudeFromEarthCenter)
 print("LEO Velocity:", iLeoVelocity)
 
-
-# In[18]:
-
-
 # Circumference of the orbit
 iLeoCircumference = 2 * math.pi * iLeoAltitudeFromEarthCenter
 print("LeoCircumference:", iLeoCircumference)
+
 # Time taken for a single orbit
 iLeoOrbitTimeTaken = iLeoCircumference/iLeoVelocity
 print("LeoOrbitTimeTaken(mins):", iLeoOrbitTimeTaken/60)
 
-
-# In[48]:
-
-
+# Num of Siteings in a day
 iDayInSecs = 24*60*60
 iNumOfSiteingsInADay = iDayInSecs/iLeoOrbitTimeTaken
 print("NumOfSiteings in a Day:", iNumOfSiteingsInADay)
 iDayInSecs/(iDayInSecs%iLeoOrbitTimeTaken)
 #0.146*103
+
+
+# ## Earth coverage
+
+# In[104]:
+
+
+# Earth Coverage and Altitude
+
+# Sin(Angle) = OppositeSide/Hypotinus
+# Cos(Angle) = AdjacentSide/Hypotinus
+# Tan(Angle) = Opposite/Adjacent
+# Opposite = Tan(Angle) * Adjacent
+# AdjacentSide = Altitude from surface
+# Angle = FieldOfView
+iSatAltitude = numpy.array([200_000, 500_000, 1_000_000, 36_000_000])
+iFieldOfView = 106 # Adjusted to sync NumOfSiteings with EarthCircumference given EarthCoverage Radius @500KM
+iEarthCoverageRadius = numpy.tan(((iFieldOfView/2)/360)*math.pi*2)*iSatAltitude
+print("EarthCoverageRadius[@{}]:{}".format(iFieldOfView, iEarthCoverageRadius))
+math.atan(iEarthRadius/(iEarthRadius+36_000_000)) * (360/(math.pi*2))
+math.atan(iEarthRadius/(iEarthRadius+500_000)) * (360/(math.pi*2))
+
+# Minimum number of orbits to cover the earths equatorial circumference
+iEarthCircumference/(iEarthCoverageRadius*2*2)
 
 
 # ## Return to Same point in Given time
@@ -86,11 +133,10 @@ iDayInSecs/(iDayInSecs%iLeoOrbitTimeTaken)
 # 
 # iOrbitRadius^3 = (iOrbitTime^2 * iGravitationalConst * iEarthMass) / (4 * Pi^2)
 
-# In[49]:
+# In[105]:
 
 
-# EvenMoreCrudeMath: rough amount of time for which satellite will be eclipsed by earth in a orbit 
-iEarthCircumference = 2 * math.pi * iEarthRadius
+# EvenMoreCrudeMath: rough amount of time for which satellite will be eclipsed by earth in a orbit
 iHalfOfEarthToLeoCircumference = (0.5*iEarthCircumference)/iLeoCircumference
 iMaxEclipseTime = iHalfOfEarthToLeoCircumference * iLeoOrbitTimeTaken
 print("Satellite could be Eclipsed by Earth for a max of {} mins per Orbit".format(iMaxEclipseTime/60))
